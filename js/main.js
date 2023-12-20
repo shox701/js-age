ebd9e447347e585fdfd49f00c106e45bc60311f7
shox701 <ebrbrn71@gmail.com>
2023-12-20 12:45:21 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 alert("Assalomu alaykum");
 var ism = prompt("Ismingizni kiriting")
 var yosh = prompt("Yoshingizni kiriting")
-if(!isNaN(yosh)){
-yosh = parseInt(yosh, 10);
 var elTitle = document.querySelector(".title")
 if(yosh > 18){
-elTitle.textContent = ism + ", oq yo'l!"
+elTitle.textContent = ism + ", oq yo'l!".toUpperCase();
 }else if(yosh < 18){
-elTitle.textContent = ism + ", sen hali yoshsan!"
-}else{
-elTitle.textContent = ism + ", oq yo'l ehtiyot bo'l!"
+elTitle.textContent = ism + ", sen hali yoshsan!".toUpperCase();
+}else if(yosh == 18){
+elTitle.textContent = ism + ", oq yo'l ehtiyot bo'l!".toUpperCase();
 }
-}else{
-    alert("Noto'g'ri yosh kiritildi! Iltimos, to'g'ri raqam kiriting!");
-}
+
 
